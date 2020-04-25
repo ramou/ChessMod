8ab9b28505b4be84def9a19acc87fc3f958cc18a
Stuart Thiel <stuart.thiel@gmail.com>
2020-04-25 00:59:00 -0400

    Fixed #8 decided I'm better off getting good textures and refining shape
     * Replaced the obsidian and glowstone with a classier tile
     * Textured the playing surface because I think it makes it look nicer
    with the new pieces

--- forge-1.14.4-28.2.0-mdk/src/main/java/com/htmlweb/chess/client/render/tileentity/WoodChessboardTileEntityRenderer.java
+++ forge-1.14.4-28.2.0-mdk/src/main/java/com/htmlweb/chess/client/render/tileentity/WoodChessboardTileEntityRenderer.java
@@ -8,7 +8,6 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockModelRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -30,8 +29,8 @@ public class WoodChessboardTileEntityRenderer extends TileEntityRenderer<WoodChe
 	public void render(final WoodChessboardTileEntity tileEntityIn, final double x, final double y, final double z, final float partialTicks, final int destroyStage) {
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
-        ResourceLocation black = new ResourceLocation("com_htmlweb_chess", "textures/block/obsidian.png");
-        ResourceLocation white = new ResourceLocation("com_htmlweb_chess", "textures/block/glowstone.png");
+        ResourceLocation black = new ResourceLocation("com_htmlweb_chess", "textures/block/black.png");
+        ResourceLocation white = new ResourceLocation("com_htmlweb_chess", "textures/block/white.png");
         
         char[][] squares = tileEntityIn.getBoardState();
 
@@ -47,11 +46,25 @@ public class WoodChessboardTileEntityRenderer extends TileEntityRenderer<WoodChe
 
          BlockModelRenderer.enableCache();
          
-         
-         bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
-
          this.bindTexture(black);
 
+         
+         
+         bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
+         double textureScale = 3;
+         GL11.glEnable(GL11.GL_BLEND); 
+         GL11.glBlendFunc(GL11.GL_SRC_COLOR, GL11.GL_ONE_MINUS_SRC_COLOR);
+         bufferbuilder.pos(x+1-3D/32D, y+1.001, z+3D/32D).tex(textureScale,textureScale).normal(0, 1, 0).endVertex();
+         bufferbuilder.pos(x+3D/32D, y+1.001, z+3D/32D).tex(textureScale,0).normal(0, 1, 0).endVertex();
+         bufferbuilder.pos(x+3D/32D, y+1.001, z+1-3D/32D).tex(0,0).normal(0, 1, 0).endVertex();
+         bufferbuilder.pos(x+1-3D/32D, y+1.001, z+1-3D/32D).tex(0,textureScale).normal(0, 1, 0).endVertex();
+         tessellator.draw();
+         GL11.glDisable(GL11.GL_BLEND);
+
+         bufferbuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_NORMAL);
+
+         
+         
          //"rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR"
          for(int by = 0; by < 8; by++) { 
         	 for(int bx = 0; bx < 8; bx++) {
@@ -155,7 +168,7 @@ public class WoodChessboardTileEntityRenderer extends TileEntityRenderer<WoodChe
 	private void drawPiece(float size, int bx, int bz, BufferBuilder bufferbuilder, final double x, final double y, final double z) {
          bufferbuilder.setTranslation(x + 2.75D/16D + bx*1.5D/16D, y +1+size, z + 2.75D/16D + bz*1.5D/16D);
 
-         double textureScale = 4*size/0.03;
+         double textureScale = 0.5*size/0.03;
          
          //south side [pos z] [parent x]
          bufferbuilder.pos(+size, -size, +size).tex(textureScale,textureScale).normal(0, 0, 1).endVertex();
@@ -168,7 +181,6 @@ public class WoodChessboardTileEntityRenderer extends TileEntityRenderer<WoodChe
          bufferbuilder.pos(-size, +size, -size).tex(textureScale,0).normal(0, 0, -1).endVertex();
          bufferbuilder.pos(+size, +size, -size).tex(0,0).normal(0, 0, -1).endVertex();
          bufferbuilder.pos(+size, -size, -size).tex(0,textureScale).normal(0, 0, -1).endVertex();
-         
 
          //east side [pos x] [parent z]
          bufferbuilder.pos(+size, -size, -size).tex(textureScale,textureScale).normal(1, 0, 0).endVertex();
@@ -182,8 +194,6 @@ public class WoodChessboardTileEntityRenderer extends TileEntityRenderer<WoodChe
          bufferbuilder.pos(-size, +size, +size).tex(textureScale,0).normal(-1, 0, 0).endVertex();
          bufferbuilder.pos(-size, +size, -size).tex(0,0).normal(-1, 0, 0).endVertex();
          bufferbuilder.pos(-size, -size, -size).tex(0,textureScale).normal(-1, 0, 0).endVertex();
-         
-
 
          //top [pos y] [parent x & y]
          bufferbuilder.pos(+size, +size, -size).tex(textureScale,textureScale).normal(0, 1, 0).endVertex();
