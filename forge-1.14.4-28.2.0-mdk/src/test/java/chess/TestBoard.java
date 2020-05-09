8617f66eb6cc560a7caaf61be76785c3b917df1e
Stuart Thiel <stuart.thiel@gmail.com>
2020-05-09 12:02:34 -0400

    Fix #37 Junit Tests now run by gradle

--- forge-1.14.4-28.2.0-mdk/src/test/java/chess/TestBoard.java
+++ forge-1.14.4-28.2.0-mdk/src/test/java/chess/TestBoard.java
@@ -1,9 +1,9 @@
 package chess;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
