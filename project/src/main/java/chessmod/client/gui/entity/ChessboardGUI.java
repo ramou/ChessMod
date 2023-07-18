27acec05fe05027cbcc26addd908f047202873de
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2023-07-18 14:29:08 -0400

    Fixing Parameters
    Switched the GuiGraphics to GuiCompenent in the paramters

--- project/src/main/java/chessmod/client/gui/entity/ChessboardGUI.java
+++ project/src/main/java/chessmod/client/gui/entity/ChessboardGUI.java
@@ -14,7 +14,7 @@ import chessmod.common.dom.model.chess.board.Board;
 import chessmod.common.dom.model.chess.piece.Piece;
 import chessmod.common.network.ChessPlay;
 import chessmod.common.network.PacketHandler;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
@@ -51,7 +51,7 @@ public abstract class ChessboardGUI extends Screen {
 		BLACK_BISHOP(4, Side.BLACK, PieceInitializer.B, 'b', new ResourceLocation(ChessMod.MODID, "textures/gui/bb.png")),
 		BLACK_PAWN(5, Side.BLACK, PieceInitializer.P, 'p', new ResourceLocation(ChessMod.MODID, "textures/gui/bp.png"));
 	
-		public void draw(GuiGraphics guiGraphics, ChessboardGUI current, int x, int y) {
+		public void draw(GuiComponent guiGraphics, ChessboardGUI current, int x, int y) {
 			current.drawPiece(guiGraphics, x, y, tile);
 		}
 		
@@ -125,46 +125,46 @@ public abstract class ChessboardGUI extends Screen {
 	}
 
 
-	protected void drawBackground(GuiGraphics guiGraphics) {
+	protected void drawBackground(GuiComponent guiGraphics) {
 		int x1 = (int)(width/2f - 128);
 		int y1 = (int)(height/2f - 128);
 		guiGraphics.blit(background, x1, y1, 0, 0, 256, 256);
 	}
 
 	
-	protected void highlightSelected(GuiGraphics guiGraphics) {
+	protected void highlightSelected(GuiComponent guiGraphics) {
 		highlightSquare(guiGraphics, selected, SELECTED);
 	}
 
-	public void highlightSquare(GuiGraphics guiGraphics, Point target, ResourceLocation shade) {
+	public void highlightSquare(GuiComponent guiGraphics, Point target, ResourceLocation shade) {
 		int x = (int)(width/2f - 128+32+target.x*24);
 		int y = (int)(height/2f - 128 + (32+target.y*24));
 
 		highlightSquare(guiGraphics, x, y, shade);
 	}
 
-	protected void highlightSquare(GuiGraphics guiGraphics, int x, int y, ResourceLocation shade) {
+	protected void highlightSquare(GuiComponent guiGraphics, int x, int y, ResourceLocation shade) {
 		highlightSquare(guiGraphics, x, y, 24, 24, shade);
 	}
 
-	protected void highlightSquare(GuiGraphics guiGraphics, int x, int y, int width, int height, ResourceLocation shade) {
+	protected void highlightSquare(GuiComponent guiGraphics, int x, int y, int width, int height, ResourceLocation shade) {
 		guiGraphics.blit(shade, x, y, width, height, width, height);
 	}
 
 
-	protected void drawPiece(GuiGraphics guiGraphics, int bx, int by, ResourceLocation piece) {
+	protected void drawPiece(GuiComponent guiGraphics, int bx, int by, ResourceLocation piece) {
 		int x1 = (int)(width/2f - 128 + 32 + bx * 24);
 		int y1 = (int)(height/2f - 128 + 32 + by * 24);
 		guiGraphics.blit(piece, x1, y1, 0, 0, 24, 24, 24, 24);
 	}
 
-	protected void drawSideboardPiece(GuiGraphics guiGraphics, TilePiece piece) {
+	protected void drawSideboardPiece(GuiComponent guiGraphics, TilePiece piece) {
 		int x1 = (int)(width/2f - 128+(piece.side.equals(Side.BLACK)?0:16+9*24));
 		int y1 = (int)(height/2f - 128 + 32 + piece.index * 24);
 		guiGraphics.blit(piece.tile, x1, y1, 0, 0, 24, 24, 24, 24);
 	}
 
-	protected void drawPieces(GuiGraphics guiGraphics) {
+	protected void drawPieces(GuiComponent guiGraphics) {
 		Board b = board.getBoard();
 	    for(int by = 0; by < 8; by++) { 
 			for(int bx = 0; bx < 8; bx++) {
