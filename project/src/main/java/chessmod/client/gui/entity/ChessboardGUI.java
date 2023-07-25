b0d5c896b68d9607c20e00317f7359ff16d1712d
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2023-07-25 11:20:29 -0400

    Need to close and re-open Gold Chessboard to change turns in single-player
    the bug is fixed in 1.20

--- project/src/main/java/chessmod/client/gui/entity/ChessboardGUI.java
+++ project/src/main/java/chessmod/client/gui/entity/ChessboardGUI.java
@@ -104,6 +104,12 @@ public abstract class ChessboardGUI extends Screen {
 		return Component.literal("");
 	}
 
+
+	@Override
+	public boolean isPauseScreen(){
+		return false;
+	}
+
 	@Override
 	public void init() {
 
