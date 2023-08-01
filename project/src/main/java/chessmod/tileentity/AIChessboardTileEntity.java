9ad97f3c6a2e43592d42c1314950f0aeeec03cd2
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2023-08-01 10:53:09 -0400

    Fix the bug of the Pieces
    The pieces are now shown in 15.2

--- project/src/main/java/chessmod/tileentity/AIChessboardTileEntity.java
+++ project/src/main/java/chessmod/tileentity/AIChessboardTileEntity.java
@@ -4,12 +4,16 @@ import chessmod.ChessMod;
 import chessmod.init.ModTileEntityTypes;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
 @Mod.EventBusSubscriber(modid = ChessMod.MODID)
 public class AIChessboardTileEntity extends ChessboardTileEntity {
 
-	public AIChessboardTileEntity(final TileEntityType<?> type) {
+	@ObjectHolder(ChessMod.MODID+ ":" + "ai_chessboard") public static TileEntityType<WoodChessboardTileEntity> TYPE;
+	public AIChessboardTileEntity(TileEntityType<?> type) {
 		super(type);
 	}
+
 
 	public AIChessboardTileEntity() {
 		super(ModTileEntityTypes.ai_chessboard);
--- project/src/main/java/chessmod/tileentity/PuzzleChessboardTileEntity.java
+++ project/src/main/java/chessmod/tileentity/PuzzleChessboardTileEntity.java
@@ -4,11 +4,14 @@ import chessmod.ChessMod;
 import chessmod.init.ModTileEntityTypes;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
 
 @Mod.EventBusSubscriber(modid = ChessMod.MODID)
 public class PuzzleChessboardTileEntity extends ChessboardTileEntity {
 
-	public PuzzleChessboardTileEntity(final TileEntityType<?> type) {
+
+	@ObjectHolder(ChessMod.MODID+ ":" + "puzzle_chessboard") public static TileEntityType<WoodChessboardTileEntity> TYPE;
+	public PuzzleChessboardTileEntity(TileEntityType<?> type) {
 		super(type);
 	}
 
