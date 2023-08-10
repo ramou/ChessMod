21cd14f27ebdc988252da711e8699266c091f95e
Stuart Thiel <stuart.thiel@gmail.com>
2023-08-10 12:53:38 -0400

    Fixed Research Milestone 1.17.1 part of #156 to move EntityRenderer association with their entities back into one of the registration classes
     * added the other board registrations

--- project/src/main/java/chessmod/setup/ClientSetup.java
+++ project/src/main/java/chessmod/setup/ClientSetup.java
@@ -1,12 +1,17 @@
 package chessmod.setup;
 
 import chessmod.client.render.blockentity.ChessboardBlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
     	event.enqueueWork(() -> {
-    		ChessboardBlockEntityRenderer.register();
-    	});
+            BlockEntityRenderers.register(Registration.WOOD_CHESSBOARD_BE.get(), ChessboardBlockEntityRenderer::new);
+            BlockEntityRenderers.register(Registration.GOLD_CHESSBOARD_BE.get(), ChessboardBlockEntityRenderer::new);
+            BlockEntityRenderers.register(Registration.AI_CHESSBOARD_BE.get(), ChessboardBlockEntityRenderer::new);
+            BlockEntityRenderers.register(Registration.CHESSES_CHESSBOARD_BE.get(), ChessboardBlockEntityRenderer::new);
+            BlockEntityRenderers.register(Registration.PUZZLE_CHESSBOARD_BE.get(), ChessboardBlockEntityRenderer::new);
+        });
     }
 }
